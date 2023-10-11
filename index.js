1fa0b71f8fe2ca39237ec8f71b91b1b3db195e50
smitht7 <smitht7@student.op.ac.nz>
2023-10-12 10:55:15 +1300

    feat: add shoe route path to the express app in index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Import the Express module
 import express from 'express';
 
-// Import routes here
+import { validateRequests } from "./middleware/validation.js";
 
+// Import routes here
+import shoeRoutes from "./routes/shoe.js";
 
 // Create an Express application
 const app = express();
 
 // Create app.use requests here
-
+app.use("/api/shoes", validateRequests(), shoeRoutes)
 
 // Start the server on port 3000
 app.listen(3000, () => {
